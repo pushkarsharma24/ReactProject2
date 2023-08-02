8a006c4478f678e2110e63071b17104caaf0c00c
Pushkar Kumar Sharma <pushkarkumarsharma2401@gmail.com>
2023-08-02 14:20:35 +0530

    Initial

---
+++ my-app/src/firebase.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD_NGKfGf1uYsS1WQnV8a_sOKJjLTiZN30",
+  authDomain: "chatapp-90851.firebaseapp.com",
+  projectId: "chatapp-90851",
+  storageBucket: "chatapp-90851.appspot.com",
+  messagingSenderId: "361326441902",
+  appId: "1:361326441902:web:cf747d9a943b1be7c2c43f"
+};
+
+
+export const app = initializeApp(firebaseConfig);
